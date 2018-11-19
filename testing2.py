eb8a7eedbbee2a8b36de52282817214aa5e1c7cd
George <georLeonard@gmail.com>
2018-11-19 03:22:15 -0800

    changing Q calculation, sync

--- testing2.py
+++ testing2.py
@@ -2,33 +2,45 @@ import gym
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
-
+# https://lilianweng.github.io/lil-log/2018/05/05/implementing-deep-reinforcement-learning-models.html
+# https://github.com/simoninithomas/Deep_reinforcement_learning_Course/blob/master/Deep%20Q%20Learning/Doom/Deep%20Q%20learning%20with%20Doom.ipynb
 env = gym.make('CartPole-v0')
 env.reset()
 sess = tf.InteractiveSession()
 
-x = tf.placeholder(tf.float32, shape=[None, 4])  # input params from gym cartpole-v0
-y_ = tf.placeholder(tf.float32, shape=[None, 1])  # output 0-1 will round to 0 or 1 to determine action
-Q = tf.placeholder(tf.float32, shape=[None, 1])
+observations = tf.placeholder(tf.float32, shape=[None, 4])  # input params from gym cartpole-v0
+actions = tf.placeholder(tf.float32, shape=[None, 2]) #placeholder for action space (left , right)
+# y_ = tf.placeholder(tf.float32, shape=[None, 765675])  #
+Q_target = tf.placeholder(tf.float32, shape=[None])
 
 # stock 5 h-layer arbitrary neuron count
-layer1 = tf.layers.dense(x, 4, tf.nn.softplus, bias_initializer=tf.random_uniform_initializer)
+layer1 = tf.layers.dense(observations, 4, tf.nn.softplus, bias_initializer=tf.random_uniform_initializer)
 layer2 = tf.layers.dense(layer1, 100, tf.nn.softplus, bias_initializer=tf.random_uniform_initializer)
 layer3 = tf.layers.dense(layer2, 100, tf.nn.softplus, bias_initializer=tf.random_uniform_initializer)
 layer4 = tf.layers.dense(layer3, 100, tf.nn.softplus, bias_initializer=tf.random_uniform_initializer)
 layer5 = tf.layers.dense(layer4, 100, tf.nn.softplus, bias_initializer=tf.random_uniform_initializer)
 
-y = tf.layers.dense(layer5, 1, tf.nn.softplus)
+output = tf.layers.dense(layer5, 2, tf.nn.softplus) #value for left and for right
+
+Q = tf.reduce_sum(tf.multiply(output, actions), axis=1)            # Q is our predicted Q value.
+
+loss = tf.reduce_mean(tf.square(Q_target-Q))
+
+optimizer = tf.train.AdamOptimizer().minimize(loss)
+
+
+
+
 
 
 # for i in range(10):
 #     sess.run(train_step)
-
-def defuzz(fuzz):
-    if fuzz < .5:
-        return 0
-    else:
-        return 0
+#
+# def defuzz(fuzz):
+#     if fuzz < .5:
+#         return 0
+#     else:
+#         return 0
 
 
 # def run_episode(env):
@@ -52,36 +64,36 @@ def defuzz(fuzz):
 #         if done:
 #             break
 # return tf.convert_to_tensor(totalreward)
-
-with tf.name_scope('cross_entropy'):
-    cross_entropy = tf.multiply(-1.0, tf.matmul(Q,Q))  # run_episode(env))
-
-
-sess.run(tf.global_variables_initializer())
-
-for i in range(100):
-
-    obs = env.reset()
-    obs = np.reshape(obs, (-1, 4))
-    totalreward = 0
-
-    action_fuzz = y.eval(feed_dict={x: obs})  # run observation through network
-
-    action = defuzz(action_fuzz)
-
-    for _ in range(201):
-        obs, reward, done, info = env.step(action)
-        obs = np.reshape(obs, (-1, 4))
-
-        action_fuzz = y.eval(feed_dict={x: obs})  # run observation through network _ repeated for each frame
-        print(action_fuzz)
-        action = defuzz(action_fuzz)
-
-        totalreward += 1
-
-        if done:
-            break
-    totalreward = np.reshape(totalreward, (-1, 1))
-    print(totalreward)
-    # sess.run(train_step, feed_dict={x: obs, Q: totalreward})
+#
+# with tf.name_scope('cross_entropy'):
+#     cross_entropy = tf.multiply(-1.0, tf.matmul(Q,Q))  # run_episode(env))
+#
+#
+# sess.run(tf.global_variables_initializer())
+#
+# for i in range(100):
+#
+#     obs = env.reset()
+#     obs = np.reshape(obs, (-1, 4))
+#     totalreward = 0
+#
+#     action_fuzz = y.eval(feed_dict={x: obs})  # run observation through network
+#
+#     action = defuzz(action_fuzz)
+#
+#     for _ in range(201):
+#         obs, reward, done, info = env.step(action)
+#         obs = np.reshape(obs, (-1, 4))
+#
+#         action_fuzz = y.eval(feed_dict={x: obs})  # run observation through network _ repeated for each frame
+#         print(action_fuzz)
+#         action = defuzz(action_fuzz)
+#
+#         totalreward += 1
+#
+#         if done:
+#             break
+#     totalreward = np.reshape(totalreward, (-1, 1))
+#     print(totalreward)
+#     sess.run(train_step, feed_dict={x: obs, Q: totalreward})
 
